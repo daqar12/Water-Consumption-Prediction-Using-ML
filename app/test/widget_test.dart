23ac91f9b7510c981c9bff6d57d1bd84a5e65c90
daqar12 <deeqcarlos@gmail.com>
2026-08-04 10:26:37 +0300

    Remove app folder from repository

--- app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:app/config/app_theme.dart';
-
-void main() {
-  test('AppColors primary matches web brand', () {
-    expect(AppColors.primary, const Color(0xFF0B1FF6));
-  });
-}
